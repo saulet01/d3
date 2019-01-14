b07789af8291662be910b1409b31e480b1b3718f
Saulet Yskak <sauletik01@gmail.com>
2019-01-14 15:28:02 -0600

    Practice: Binding (enter.append && exit.remove)

--- d3/app.js
+++ d3/app.js
@@ -1,42 +1,27 @@
-let container = d3.select(".wrap")
+let container = d3.select("#wrap")
+let clients = [{
+  Name: "Client0"
+}]
+let count = 1;
 
-d3.csv("data.csv").then(dataLoaded)
-
-function dataLoaded(data){
-  showData(data)
-  statistics(data)
-}
-
-function showData(data){
-  for(let i of data){
-    write(i.Name + ", " + i.Position + ", "+i.Age)
-  }
-}
-
-function statistics(data){
-  let total = d3.sum(data, (i) => {
-    return parseInt(i.Age)
+function addClient(){
+  clients.push({
+    Name: "Client" + count
   })
-  total = `The sum is ${total}`
-  header(total)
-
-  let mean = d3.mean(data, (i) => {
-    return parseInt(i.Age)
-  })
-  mean = `The Mean is ${mean}`
-  header(mean)
-
-  let extent = d3.extent(data, (i) => {
-    return parseInt(i.Age)
-  })
-  extent = `The maximum and minimum is ${extent}`
-  header(extent)
+  count = count + 1
+  showData(clients)
 }
 
-const write = (text) => {
-  container.append("div").text(text)
+function removeClient(){
+  clients = clients.slice(0, -1) //not show the last one
+  count = count - 1
+  showData(clients)
 }
 
-const header = (text) => {
-  container.append("h3").text(text)
+function showData(clients){
+  let join = container.selectAll("div").data(clients);
+  join.enter().append("div").text(d => d.Name + " - New")
+  join.exit().remove()
+  join.text(d => d.Name + ": Updated")
 }
+showData(clients)
